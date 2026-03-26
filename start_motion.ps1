07d590ddc3645bdb5fa315db8a8740953be892b2
Antoine Hilberer <57757205+AHilberer@users.noreply.github.com>
2026-03-26 23:51:37 +0100

    slight change to powershell server launchers

--- start_motion.ps1
+++ start_motion.ps1
@@ -1,9 +1,8 @@
-cd C:\Users\HPS DYN\Documents\IRLab_control
-.\venv\Scripts\activate
+.\.venv\Scripts\activate
 
 Write-Host "-----------------------------------------------------"
 Write-Host "Starting motion server ..."
 Write-Host "-----------------------------------------------------"
 
-uvicorn servers.motion_server:app --host 192.168.1.2 --port 8001
+uvicorn servers.motion_server:app --host 192.168.1.3 --port 8001
 
--- start_spectro.ps1
+++ start_spectro.ps1
@@ -1,9 +1,8 @@
-cd C:\Users\HPS DYN\Documents\IRLab_control
-.\venv\Scripts\activate
+.\.venv\Scripts\activate
 
 Write-Host "-----------------------------------------------------"
 Write-Host "Starting spectro server ..."
 Write-Host "-----------------------------------------------------"
 
-uvicorn servers.spectro_server:app --host 192.168.1.2 --port 8002
+uvicorn servers.spectro_server:app --host 192.168.1.3 --port 8002
 
